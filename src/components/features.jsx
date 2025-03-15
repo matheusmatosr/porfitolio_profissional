3095a9a64ecb75e04a3246438db4fccac5141653
Matheus Matos <100291312+matheusmatosr@users.noreply.github.com>
2025-03-15 08:08:25 -0300

    fix: ajustes

--- src/components/features.jsx
+++ src/components/features.jsx
@@ -7,7 +7,7 @@ export const Features = (props) => {
         <div className="col-md-10 col-md-offset-1 section-title">
           <h2 style={{ marginTop: "45px" }}>Características</h2>
         </div>
-        <div className="row">
+        <div className="row" style={{ marginBottom: "20px" }}>
           {props.data
             ? props.data.map((d, i) => (
                 <div key={`${d.title}-${i}`} className="col-xs-6 col-md-3">
